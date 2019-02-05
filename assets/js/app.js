4e1bd0fd894b9a68269d5c4d916d034df7a7e2f5
Damien <dbournac@yahoo.fr>
2019-02-05 08:42:50 +0100

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+import '../css/app.scss';
+import 'bootstrap';
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+UIkit.use(Icons);
+import $ from 'jquery';
+global.$ = global.jQuery = $;
